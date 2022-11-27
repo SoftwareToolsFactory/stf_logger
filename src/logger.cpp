a57925344c699d27d841766847bca6d3cda477ef
Nuroferatu <32645518+Nuroferatu@users.noreply.github.com>
2022-11-27 16:00:17 +0100

    Color mapping for linux

--- src/logger.cpp
+++ src/logger.cpp
@@ -69,6 +69,8 @@ Log::~Log() {
 #pragma endregion
 
 // Color control
+#ifdef _WIN32
+
 Logger& Logger::setColor( const logutils::Color& color ) {
     switch( color ) {
         // case logutils::Color::eReset:   setConsoleNormalColor(); break;
@@ -92,9 +94,36 @@ Logger& Logger::setColor( const logutils::Color& color ) {
     return *this;
 }
 
+#elif __linux__
+
+Logger& Logger::setColor( const logutils::Color& color ) {
+    switch( color ) {
+        case logutils::Color::eReset:   std::cout << "\033[0;0m"; break;
+
+        case logutils::Color::eRed:             std::cout << "\033[0;31m"; break;
+        case logutils::Color::eGreen:           std::cout << "\033[0;32m"; break;
+        case logutils::Color::eYellow:          std::cout << "\033[0;33m"; break;
+        case logutils::Color::eBlue:            std::cout << "\033[0;34m"; break;
+        case logutils::Color::eMagneta:         std::cout << "\033[0;35m"; break;
+        case logutils::Color::eCyan:            std::cout << "\033[0;36m"; break;
+        case logutils::Color::eWhite:           std::cout << "\033[0;37m"; break;
+
+        case logutils::Color::eIntenseRed:      std::cout << "\033[1;31m"; break;
+        case logutils::Color::eIntenseGreen:    std::cout << "\033[1;32m"; break;
+        case logutils::Color::eIntenseYellow:   std::cout << "\033[1;33m"; break;
+        case logutils::Color::eIntenseBlue:     std::cout << "\033[1;34m"; break;
+        case logutils::Color::eIntenseMagneta:  std::cout << "\033[1;35m"; break;
+        case logutils::Color::eIntenseCyan:     std::cout << "\033[1;36m"; break;
+        case logutils::Color::eIntenseWhite:    std::cout << "\033[1;37m"; break;
+    }
+    return *this;
+}
+
+#endif
+
 // Global logger to make usage as simple as cout
 Log    theLog( "theLog" );
 
-}
+} //ns:stf
 
 // vim: ts=4:sw=4:et:nowrap
